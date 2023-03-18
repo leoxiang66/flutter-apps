aceca14f9e0e1e06ebbbbf278998464bb33414cc
Tao Xiang <xiang.tao@outlook.de>
2023-03-18 11:24:46 +0100

    1

--- peer/lib/main.dart
+++ peer/lib/main.dart
@@ -1,109 +1,58 @@
 import 'package:flutter/material.dart';
+import 'pages/home.dart' show HomePage;
+import 'pages/about.dart' show AboutPage;
 
+/// This is the main function of the app.
 void main() {
   runApp(MyApp());
 }
 
+/// This is the main widget of the app.
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter App',
+      title: 'PEER - Paper Evaluation and Empowerment Resource',
       home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+/// This is the home page widget of the app.
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var selectedIndex = 0;
+
+  void handleMenuItemTapped(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    Size screenSize = MediaQuery.of(context).size;
-    double imagePaddingValue = screenSize.width * 0.05; // Set image padding to 5% of screen width
+    
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Center(child: Text('PEER', style: TextStyle(color: Colors.white))),
-        backgroundColor: Colors.red, // Change the AppBar color to red
-        leading: Builder(
-          builder: (BuildContext context) {
-            return IconButton(
-              icon: Icon(Icons.menu),
-              onPressed: () {
-                Scaffold.of(context).openDrawer();
-              },
-            );
-          },
-        ),
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            SizedBox(
-            height: 60, // Set the desired height of the DrawerHeader
-              child: DrawerHeader(
-                child: Center(child: Text(
-            'PEER',
-            style: TextStyle(
-              color: Colors.white, // Set the text color to white
-              fontWeight: FontWeight.bold, // Set the font weight to bold
-            ),
-          ),),
-                decoration: BoxDecoration(
-                  color: Colors.red,
-                ),
-              ),
-            ),
-            ListTile(
-              title: Text('About'),
-              onTap: () {
-                // Add your action here
-              },
-            ),
-            ListTile(
-              title: Text('GitHub'),
-              onTap: () {
-                // Add your action here
-              },
-            ),
-          ],
-        ),
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.fromLTRB(imagePaddingValue, 0, imagePaddingValue, 0), // Set top and bottom padding to 0
-          child: Align(
-            alignment: Alignment.topCenter,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                LayoutBuilder(
-                  builder: (BuildContext context, BoxConstraints constraints) {
-                    return Image.network(
-                      'https://raw.githubusercontent.com/Kasneci-Lab/AI-assisted-writing/ui/img/Peer_logo.png',
-                      width: constraints.maxWidth,
-                      fit: BoxFit.cover,
-                    );
-                  },
-                ),
-                SizedBox(height: imagePaddingValue),
-                Text('PEER (Paper Evaluation and Empowerment Resource) is a cutting-edge application designed to empower researchers, students, and academics in their scholarly pursuits. This innovative platform offers a suite of powerful tools and resources that make it easy to evaluate, critique, and improve research papers, ensuring that they meet the highest standards of quality and rigor. Whether you are a seasoned researcher looking to refine your work, or a student just starting out on your academic journey, PEER is an invaluable resource that can help you achieve your goals and excel in your field. With its user-friendly interface and powerful features, PEER is the ultimate tool for anyone looking to take their research to the next level.'), // Add a text description below the image
-                SizedBox(height: imagePaddingValue),
-                ElevatedButton(
-                  onPressed: () {
-                    // Add your action here
-                  },
-                child: Text('Get Started', style: TextStyle(fontSize: 18)),
-                  style: ButtonStyle(
-                minimumSize: MaterialStateProperty.all(Size(screenSize.width * 0.4, screenSize.width * 0.15)),
-              ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
+    Widget page;
+      switch (selectedIndex) {
+        case 0:
+          page = HomePage(onMenuItemTapped: handleMenuItemTapped,);
+          break;
+        case 1:
+          page = AboutPage(onMenuItemTapped: handleMenuItemTapped,);
+          break;
+        default:
+          throw UnimplementedError('no widget for $selectedIndex');
+      } 
+
+    return page;
   }
 }
+
+
+
+
