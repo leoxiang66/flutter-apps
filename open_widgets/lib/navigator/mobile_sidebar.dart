80a24c0e76ff51c83d8cd139d33f4074b078f5c8
Tao Xiang <xiang.tao@outlook.de>
2023-03-19 20:24:45 +0100

    1

--- open_widgets/lib/navigator/mobile_sidebar.dart
+++ open_widgets/lib/navigator/mobile_sidebar.dart
@@ -1,55 +1,75 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:hovering/hovering.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class OpenSidebar extends StatefulWidget {
-  @override
-  _OpenSidebarState createState() => _OpenSidebarState();
-}
 
-class _OpenSidebarState extends State<OpenSidebar> {
+class OpenSidebar extends StatelessWidget {
+  final List<OpenSidebarItem> children;
+
+  OpenSidebar({required this.children});
+
   @override
   Widget build(BuildContext context) {
     return Container(
       width: 60,
-      color: Colors.grey.shade800,
-      child: ListView(
-        children: [
-          OpenSidebarItem(icon: FontAwesomeIcons.home, tooltip: "Home"),
-          OpenSidebarItem(icon: FontAwesomeIcons.user, tooltip: "Profile"),
-          OpenSidebarItem(icon: FontAwesomeIcons.search, tooltip: "Search"),
-          OpenSidebarItem(icon: FontAwesomeIcons.cog, tooltip: "Settings"),
-        ],
+      color: Colors.white,
+      child: Column(
+        children: children
+            .map((item) => Padding(
+                  padding: EdgeInsets.symmetric(vertical: 25),
+                  child: item,
+                ))
+            .toList(),
       ),
     );
   }
 }
 
-class OpenSidebarItem extends StatelessWidget {
-  final IconData icon;
+// import 'package:flutter/material.dart';
+// import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class OpenSidebarItem extends StatefulWidget {
+  final Widget icon;
   final String tooltip;
 
   OpenSidebarItem({required this.icon, required this.tooltip});
 
   @override
+  _OpenSidebarItemState createState() => _OpenSidebarItemState();
+}
+
+class _OpenSidebarItemState extends State<OpenSidebarItem> {
+  bool _isHovered = false;
+
+  void _onEnter(PointerEnterEvent event) {
+    setState(() {
+      _isHovered = true;
+    });
+  }
+
+  void _onExit(PointerExitEvent event) {
+    setState(() {
+      _isHovered = false;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(vertical: 12),
-      child: HoverCrossFadeWidget(
-        firstChild: Icon(icon, color: Colors.white),
-        secondChild: Tooltip(
-          message: tooltip,
-          child: Icon(icon, color: Colors.white),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(5),
-            color: Colors.blue,
-          ),
-          textStyle: TextStyle(color: Colors.white),
-          verticalOffset: 20,
-          preferBelow: false,
-        ),
-        duration: Duration(milliseconds: 200),
-      ),
+    return MouseRegion(
+      onEnter: _onEnter,
+      onExit: _onExit,
+      child: _isHovered
+          ? Tooltip(
+              message: widget.tooltip,
+              child: widget.icon,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(5),
+                color: Colors.blue,
+              ),
+              textStyle: TextStyle(color: Colors.white),
+              verticalOffset: 20,
+              preferBelow: true,
+            )
+          :widget.icon,
     );
   }
 }
