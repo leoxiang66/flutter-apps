632a0d1ed715e602f18aeaaf3c40b5c870a12ac7
Tao Xiang <46348901+leoxiang66@users.noreply.github.com>
2023-04-13 14:46:31 +0200

    1

--- open_widgets/lib/input/text_input.dart
+++ open_widgets/lib/input/text_input.dart
@@ -5,12 +5,15 @@ class OpenTextInput extends StatefulWidget {
   final void Function(String) onChanged;
   final String label;
   final double width;
+  final int minLines = 1;
+  final int? maxLines = 1;
 
   const OpenTextInput(
       {super.key,
       required this.onSubmitted,
       required this.onChanged,
-      required this.label, required this.width});
+      required this.label,
+      required this.width});
 
   @override
   _OpenTextInputState createState() => _OpenTextInputState();
@@ -30,6 +33,8 @@ class _OpenTextInputState extends State<OpenTextInput> {
     return SizedBox(
       width: widget.width,
       child: TextField(
+        minLines: widget.minLines,
+        maxLines: widget.maxLines,
         controller: _controller,
         decoration: InputDecoration(
           labelText: widget.label,
