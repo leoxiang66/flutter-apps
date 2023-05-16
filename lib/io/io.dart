ae28b6317a98faa86df3a879eb73d0b3a2383437
Tao Xiang <xiang.tao@outlook.de>
2023-05-16 22:19:56 +0200

    Update io.dart

--- lib/io/io.dart
+++ lib/io/io.dart
@@ -22,3 +22,25 @@ Future<void> writeTOMLAsync(Map<String, dynamic> config, String TOMLPath) async 
   await File(TOMLPath).writeAsString(toml);
 }
 
+Map<String, dynamic> readMapFromTOMLSync(String TOMLPath) {
+  File configFile = File(TOMLPath);
+  if (configFile.existsSync()) {
+    String contents = configFile.readAsStringSync();
+    TomlDocument document = TomlDocument.parse(contents);
+    return document.toMap();
+  } else {
+    throw Exception('File does not exist at path: $TOMLPath');
+  }
+}
+
+Future<Map<String, dynamic>> readMapFromTOMLAsync(String TOMLPath) async {
+  File configFile = File(TOMLPath);
+  if (await configFile.exists()) {
+    String contents = await configFile.readAsString();
+    TomlDocument document = TomlDocument.parse(contents);
+    return document.toMap();
+  } else {
+    throw Exception('File does not exist at path: $TOMLPath');
+  }
+}
+
