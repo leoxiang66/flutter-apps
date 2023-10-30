536ff2b98836e180ffd99f4eddd3b080e5361994
Tao Xiang <46348901+leoxiang66@users.noreply.github.com>
2023-10-31 01:13:27 +0800

    1

--- lib/navigator/utils.dart
+++ lib/navigator/utils.dart
@@ -33,11 +33,16 @@ void go_to_internal_page(BuildContext context, Widget page, {bool pushonly = fal
   
 }
 
-Future<void> go_to_url(String url) async {
+Future<void> go_to_url(String url, {bool intab = false}) async {
   final Uri _url = Uri.parse(url);
 
   try {
+    if(!intab){
     await launchUrl(_url);
+    }
+    else{
+    await launchUrl(_url, webOnlyWindowName: "_self");
+    }
   } on PlatformException catch (e) {
     logger.e('无法打开 URL，错误信息：$e');
   } on UnsupportedError catch (e) {
