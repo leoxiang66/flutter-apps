e901488ab7882c49a81c6e9ae2529e20f2edaad9
Tao Xiang <46348901+leoxiang66@users.noreply.github.com>
2024-03-16 14:42:05 +0800

    1

---
+++ my_flutter_project/lib/state.dart
@@ -0,0 +1,22 @@
+// import 'package:flutter/material.dart';
+
+// class MyAppState extends ChangeNotifier {
+//   // 全局变量
+//   var current = WordPair.random();
+//   var favorites = <WordPair>[];
+
+//   // callbacks
+//   void getNext() {
+//     current = WordPair.random();
+//     notifyListeners();
+//   }
+
+//   void toggleFavorite() {
+//     if (favorites.contains(current)) {
+//       favorites.remove(current);
+//     } else {
+//       favorites.add(current);
+//     }
+//     notifyListeners();
+//   }
+// }
